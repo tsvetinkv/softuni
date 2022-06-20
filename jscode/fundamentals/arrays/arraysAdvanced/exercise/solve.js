59eac1efb271c0922f352760ad22e297fac70c56
Цвети Николова <99092833+tsvetinkv@users.noreply.github.com>
2022-06-20 18:25:06 +0300

    aded new prolem

---
+++ jscode/fundamentals/arrays/arraysAdvanced/exercise/solve.js
@@ -0,0 +1,20 @@
+function searchforANumber(arr1 ,arr2) {
+let numOfElements = arr2.shift();
+let deleteCount = arr2.shift();
+let searchedNum = arr2.shift();
+let newArr = arr1.slice(0, numOfElements)
+newArr.splice(0,deleteCount)
+let counter = 0;
+console.log(newArr);
+for (let el of newArr) {
+    if
+  let i = newArr.indexOf(searchedNum)
+  console.log(i); 
+}
+}searchforANumber([5, 2, 3, 4, 1, 6],
+
+    [5, 2, 3]);
+
+
+
+// "Number {number} occurs {count} times."
