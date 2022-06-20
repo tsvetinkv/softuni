794fc50399e4495773f78f4dae821045b4bb8002
Цвети Николова <99092833+tsvetinkv@users.noreply.github.com>
2022-06-20 14:10:09 +0300

    cahnege bomb numbers

--- jscode/fundamentals/arrays/arraysAdvanced/exercise/bombNumbers.js
+++ jscode/fundamentals/arrays/arraysAdvanced/exercise/bombNumbers.js
@@ -1,46 +1,27 @@
 function bombNumbers(sequence, bombAndPower) {
-  let bomb = bombAndPower.shift();
-  let power = bombAndPower.shift();
-  let index = sequence.indexOf(bomb);
-
-  if (countInArray(sequence, bomb) > 1){
-    let index2 = sequence.indexOf(bomb);  
-
-  let sum = 0;
-  let res = sequence.slice(index + 2, index2 - power)
-  for (const el of res) {
-    sum += el
-  }
-  let result = sequence.slice(index2 + power + 1, sequence.length)
-  for (const el of result) {
-  sum += el
-  }
-
-  }
- 
-
-  let sum = 0;
-   let res = sequence.slice(0, index - power)
-for (const el of res) {
-  sum += el
+    let bomb = bombAndPower.shift();
+    let power = bombAndPower.shift();
+    for (let i = 0; i < sequence.length; i++) {
+        if(sequence[i] == bomb) {
+deleteNext(sequence, i, power);
+        }
+   
+    }
+  
+    function deleteNext(arr, index, power) {
+let start = Math.max(0,index - power);
+let end= Math.min(arr.length - 1, index + power);
+for (let i = start; i <= end; i++) {
+    arr[i] = 0;
 }
-let result = sequence.slice(index + power + 1, sequence.length)
-for (const el of result) {
-sum += el
+    }
+    let sum= 0;
+    for (const el of sequence) {
+        sum += el
+    }
+    console.log(sum);
 }
-console.log(sum);
 
-
-function countInArray(array, el) {
-  let count = 0;
-  for (let i = 0; i < array.length; i++) {
-      if (array[i] === el) {
-          count++;
-      }
-  }
-  return count;
-}
-}
-//bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]);
-//bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2])
-bombNumbers([1, 1, 2, 1, 1, 1,2, 1, 1, 1],[2, 1])
+  bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]);
+  bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2])
+  bombNumbers([1, 1, 2, 1, 1, 1,2, 1, 1, 1],[2, 1])
