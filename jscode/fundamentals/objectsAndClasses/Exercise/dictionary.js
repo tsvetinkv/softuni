4995d9024d0a6d45d518b6adf10ee94e08934e28
tsvetinkv <cnikolova95@gmail.com>
2022-07-04 16:32:57 +0300

    contact book web app

--- jscode/fundamentals/objectsAndClasses/Exercise/dictionary.js
+++ jscode/fundamentals/objectsAndClasses/Exercise/dictionary.js
@@ -1,29 +1,13 @@
 function dictionary(jsonArray) {
-    let words = [];
-    let dict = {};
-  for (let word of jsonArray) {
-    let dictionary = JSON.parse(word);
-dict.term = dictionary;
-  console.log(dict.term);
- let split = word.split(":")
- //console.log(split);
-//  let remove =  split[0].replace('{"', "");
-//  let firstWord = remove.replace('"', "")
-//  //console.log(firstWord);
-//  dict.term = firstWord;
- let removeFromDef = split[1].replace('"}', "")
- let definition = removeFromDef.replace('"', "")
- //console.log(definition)
- dict.definition = definition;
-  words.push(dict.term);
-  //console.log(words[0]);
-    // words.sort((a, b) => a.localeCompare(b));
-    // console.log(dict.term);
-   
-   //console.log(dict)
-  } 
-
+  let dictionary ={};
+ jsonArray.forEach((element) => Object.assign(dictionary,JSON.parse(element)));
+ let sortedKeys = Object.keys(dictionary).sort((a, b) => a.localeCompare(b));
+ sortedKeys.forEach((term) => {
+ let definition = dictionary[term];
+ console.log(`Term: ${term} => Definition: ${definition}`);
+  });
 }
+
 
 dictionary([
   '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
@@ -33,3 +17,24 @@ dictionary([
   '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
 ]);
 //Term: Boiler => Definition: A fuel-burning apparatus or container for heating water.
+
+
+// function dictionary(jsonArray) {
+//   // създаваме си един празен обект който след това ще ни държи стойностите на всички елементи от json масива
+// const dictionary = {};
+// // JSON.parse ни създава обект от JSON string
+// // ключовете и валютата от всеки обект ги добавяме към dictionary s JSON.assign
+// jsonArray.forEach((element) => Object.assign(dictionary,JSON.parse(element)));
+
+// // Object.keys() ни връща всичките ключове към вече напълнения със стойности dictionary обект и след това ги сортираме
+// const sortedKeys = Object.keys(dictionary).sort((a, b) => a.localeCompare(b));
+// // => ["Boiler", "Bus" ...]
+
+//   // Минаваме през всички елементи от масива със сортирани ключове
+// sortedKeys.forEach((term) => {
+//   // dictionary[term] е като все едно да направим dictionary["Boiler"] или dictionary.Boiler
+//   // защото term ни е от елемент от sortedKeys
+//   const definition = dictionary[term];
+//   console.log(`Term: ${term} => Definition: ${definition}`);
+// });
+// }
